ad0c86c89b6c247439e3bd1e3fe6740e39b484e3
jdeluna06 <jdeluna0672@gmail.com>
2024-11-29 13:05:49 -0800

    schedule models code

---
+++ server/src/models/Schedule.ts
@@ -0,0 +1,55 @@
+import { Schema, model, type Document, Types } from "mongoose";
+
+interface ISchedule extends Document {
+  date: string;
+  startTime: string;
+  endTime: string;
+  description: string | null;
+  dateId: Types.ObjectId;
+  location: string | null;
+}
+
+const scheduleSchema = new Schema<ISchedule>(
+  {
+    date: {
+      type: String,
+      required: true,
+      },
+    startTime: {
+      type: String,
+      required: true,
+      
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    dateId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    location: {
+      type: String,
+      required: false,
+    },
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+
+
+
+
+const Schedule = model<ISchedule>("Schedule", scheduleSchema);
+
+export default Schedule;
